da60258cb2de84450f6f04249a0b243a9a244cb3
Sandip Gupta <sg02295@gmail.com>
2024-10-14 23:14:49 +0530

    ui added

--- src_final/src/app/model/cab.model.ts
+++
@@ -1,9 +0,0 @@
-// src/app/models/cab.model.ts
-
-export interface Cab {
-    cabId: number;
-    cabName: string;
-    dateOfBook: string; // Use 'string' to manage date as a string, or use 'Date' type with proper formatting
-    driverId: number;
-  }
-  
--- src_final/src/app/model/driver.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Driver {
-    driverId: number;
-    driverName: string;
-    fare: number;    // Fare field
-    rating: number;  // Rating field
-  }
-  
